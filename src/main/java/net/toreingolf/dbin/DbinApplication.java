aa6178abce3ca6aa3fbe0b6cba186e98ed475bad
Tore I. Christoffersen <toreingolf@yahoo.no>
2025-09-03 09:57:10 +0200

    Initial Spring Boot application

---
+++ src/main/java/net/toreingolf/dbin/DbinApplication.java
@@ -0,0 +1,13 @@
+package net.toreingolf.dbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/toreingolf/dbin/DbinApplicationTests.java
@@ -0,0 +1,13 @@
+package net.toreingolf.dbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
